e47b878dc1ec2879066f407c3db7fbed6063ae24
ayush-chothe <53037930+ayush-chothe@users.noreply.github.com>
2020-04-02 00:35:09 +0530

    Created TankPlayerController and TankAIController (C++)

---
+++ Source/BattleTank/TankPlayerController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is not possessing the Tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController is possessing: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
